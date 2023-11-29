e1e0ba190f5984f4ae6f74a11ab18843faf7a700
Raúl Fernando Medina Sandoval <raulmedinasandoval@gmail.com>
2023-11-29 11:16:31 -0500

    actualiza clased baco y clase cuenta

--- Ejercicio7/Banco.cs
+++ Ejercicio7/Banco.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+//** Autor:Ráúl Fernando Medina Sandoval
+//** Fecha: 28/11/2023
+//** Descripcion: La clase Banco representa una entidad bancaria
+//** y gestiona un conjunto de operaciones relacionadas con la gestión de cuentas, transferencias y autenticación de usuarios.
 
 public class Banco
     {
+    // Propiedades de la clase Banco
     public int id { get; private set; }
     public String name { get; private set; }
     public Double topeRetiro { get; private set; }
     public int pesosPuntos { get; private set; }
     public List<Cuenta> cuentas = new List<Cuenta>();
-
-public Banco(int id, string name, double topeRetiro, int pesosPuntos)
+    
+    // Constructor de la clase Banco
+    public Banco(int id, string name, double topeRetiro, int pesosPuntos)
     {
         this.id = id;
         this.name = name;
@@ -22,7 +30,8 @@ public Banco(int id, string name, double topeRetiro, int pesosPuntos)
         this.pesosPuntos = pesosPuntos;
         
     }
-    public Cuenta obtenerCuenta(String cuenta)
+    // Método para obtener un numero de cuenta
+      public Cuenta obtenerCuenta(String cuenta)
     {
         foreach ( Cuenta numCuenta in cuentas)
         {
@@ -33,7 +42,8 @@ public Banco(int id, string name, double topeRetiro, int pesosPuntos)
         }
       return new Cuenta();
     }
-        public Boolean transferencia (String origen, String destino, Double valor)
+    // Método para realizar una transferencia entre  cuentas
+    public Boolean transferencia (String origen, String destino, Double valor)
     {
        Cuenta cuentaOrigen = obtenerCuenta(origen);
        Cuenta cuentaDestino = obtenerCuenta(destino);
@@ -42,42 +52,50 @@ public Banco(int id, string name, double topeRetiro, int pesosPuntos)
             if (cuentaOrigen.retiro(valor))
             {
                 cuentaDestino.consignar(valor);
-                return true;
+                return true; // La transferencia fue exitosa
             }
           }
-        return false;
+        return false;// La transferencia No exitosa
     }
+    // Método para agregar una cuenta a la lista de cuentas del banco
     public void agregarCuenta(Cuenta cuenta)
     {
         this.cuentas.Add(cuenta);
     }
+    // Método para autenticar una cuenta por número y clave
     public Boolean autenticar(String cuenta, String clave)
     {
         foreach (Cuenta numCuenta in cuentas)
         {
-          numCuenta.isLoggin = false;
+          numCuenta.isLoggin = false; // Se inicializa el estado de login como falso para todas las cuentas
         }
         foreach (Cuenta numCuenta in cuentas)
         {
             if (numCuenta.cuenta.Equals(cuenta) && numCuenta.clave.Equals(clave))
             {
-                numCuenta.isLoggin = true;
-                return true;
+                numCuenta.isLoggin = true; // Se marca la cuenta como logueada si la autenticación es exitosa
+                return true; // Autenticación exitosa
             }
         }
-        return false;
+        return false; // Autenticación fallida
     }
+    // Método que vevuelve la cuenta que está actualmente logueada
     public Cuenta cuentaLogeada()
     {
         foreach ( Cuenta numCuenta in cuentas)
         {
             if (numCuenta.isLoggin)
             {
-            return numCuenta;
+            return numCuenta; // Devuelve la cuenta logueada
             } 
         }
         return null;
     }
+    public Boolean validarTope(Double valor)
+    {
+        return valor <= topeRetiro;
+    }
+
 }
 
 
--- Ejercicio7/Cuenta.cs
+++ Ejercicio7/Cuenta.cs
@@ -5,16 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-    public class Cuenta
+//** Autor:Ráúl Fernando Medina Sandoval
+//** Fecha: 28/11/2023
+//** Descripcion: La clase Cuenta representa una cuenta bancaria y proporciona métodos para 
+//** realizar operaciones como redimir puntos, consignar, retirar, y consultar saldo y puntos.
+//**También gestiona el estado de inicio de sesión para la cuenta.
+public class Cuenta
     {
+    // Propiedades de la clase Cuenta
     public Double saldo { get; private set; }
     public int puntos { get; private set; }
     public String usuario { get; private set; }
     public String clave { get; private set; }
     public String cuenta { get; private set; }
     public Boolean isLoggin { get; set; }
-
+    // Constructor de la clase Cuenta
     public Cuenta(double saldo, int puntos, string usuario, String clave, string cuenta)
     {
         this.saldo = saldo;
@@ -24,6 +29,7 @@ using System.Threading.Tasks;
         this.cuenta = cuenta;
         this.isLoggin = false;
     }
+    // Constructor  de la clase cuenta por defecto
     public Cuenta() { }
     public Double redimirPuntos(int puntos, Double pesosPuntos)
     {
@@ -32,27 +38,34 @@ using System.Threading.Tasks;
         this.puntos -= puntos;
         return result;
     }
+    // Método para verificar si es posible redimir la cantidad de puntos especificada
     public Boolean puedeRedimir(int puntos)
     {
-        return this.puntos >= puntos;   
+        return this.puntos >= puntos;   // Devuelve true si la cuenta tiene suficientes puntos para redimir, de lo contrario, false 
     }
+
+    // Método para consignar un valor a la cuenta
     public void consignar( Double valor)
-    {
-        this.saldo += valor;
+        {
+        this.saldo += valor; // Aumenta el saldo de la cuenta con el valor consignado
     }
+    // Método para realizar un retiro de la cuenta
     public Boolean retiro(Double valor)
     {
         if (this.saldo >= valor)
         {
-            this.saldo -= valor;
-            return true;
+            this.saldo -= valor;   // Realiza el retiro si hay suficientes fondos en la cuenta
+            return true;   // Indica que el retiro fue exitoso
         }
-        return false;
+        return false;    // Indica que el retiro no fue posible por falta de fondos
     }
+   
+    // Método para consultar el saldo actual de la cuenta
     public Double consultarSaldo()
     {
         return this.saldo;
     }
+   
     public int consultarPuntos()
     {
         return this.puntos;
